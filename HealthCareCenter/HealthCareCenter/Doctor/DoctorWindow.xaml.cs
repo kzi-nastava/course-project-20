6606e8e9bfcd9b763f5c435df33fce8f25880d75
MilosS019 <milosstanojlovic019@gmail.com>
2022-04-23 18:17:31 +0200

    Created a new window that lists all apointments and started working on funcionality 2.1

--- HealthCareCenter/HealthCareCenter/Doctor/DoctorWindow.xaml.cs
+++ HealthCareCenter/HealthCareCenter/Doctor/DoctorWindow.xaml.cs
@@ -13,16 +13,73 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using HealthCareCenter.Model;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace HealthCareCenter
 {
     public partial class DoctorWindow : Window
     {
-        public Doctor signedUser;
+        private Doctor signedUser;
+        private DataTable dataTable;
+        private bool tableIsFilled = false;
+        private List<Appointment> Appointments;
         public DoctorWindow(Model.User user)
         {
             signedUser = (Doctor) user;
             InitializeComponent();
+        }
+
+        private void scheduleRewiewButton_Click(object sender, RoutedEventArgs e)
+        {
+            startingGrid.Visibility = Visibility.Collapsed;
+            scheduleGrid.Visibility = Visibility.Visible;
+            if (tableIsFilled)
+            {
+                return;
+            }
+            dataTable = new DataTable("emp");
+
+            DataColumn dc1 = new DataColumn("Id", typeof(int));
+            DataColumn dc2 = new DataColumn("Type of appointment", typeof(string));
+            DataColumn dc3 = new DataColumn("Appointment date", typeof(string));
+            DataColumn dc4 = new DataColumn("Creation date", typeof(string));
+            DataColumn dc5 = new DataColumn("Emergency", typeof(bool));
+            DataColumn dc6 = new DataColumn("Doctors first and last name", typeof(string));
+            DataColumn dc7 = new DataColumn("Room", typeof(string));
+            dataTable.Columns.Add(dc1);
+            dataTable.Columns.Add(dc2);
+            dataTable.Columns.Add(dc3);
+            dataTable.Columns.Add(dc4);
+            dataTable.Columns.Add(dc5);
+            dataTable.Columns.Add(dc6);
+            dataTable.Columns.Add(dc7);
+            AppointmentsMenager appointmentsMenager = new AppointmentsMenager();
+            List<Appointment> appointments = appointmentsMenager.loadAppointments();
+            DataRow dr;
+            foreach(Appointment appointment in appointments)
+            {
+                dr = dataTable.NewRow();
+                dr[0] = appointment.ID;
+                dr[1] = appointment.Type;
+                dr[2] = appointment.AppointmentDate;
+                dr[3] = appointment.CreatedDate;
+                dr[4] = appointment.Emergency;
+                dr[5] = appointment.DoctorID;
+                dr[6] = appointment.HospitalRoomID;
+                dataTable.Rows.Add(dr);
+            }
+            scheduleDataGrid.ItemsSource = dataTable.DefaultView;
+        }
+
+        private void drugMenagmentButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ HealthCareCenter/HealthCareCenter/Model/AppointmentsMenager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using HealthCareCenter.Model;
+using Newtonsoft.Json;
+
+namespace HealthCareCenter.Model
+{
+    internal class AppointmentsMenager
+    {
+        public List<Appointment> Appointments { get; set; }
+        public List<Appointment> loadAppointments()
+        {
+            var settings = new JsonSerializerSettings
+            {
+                DateFormatString = Constants.DateFormat
+            };
+
+            String JSONTextAppointments = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\data\appointments.json");
+            Appointments = (List<Appointment>)JsonConvert.DeserializeObject<IEnumerable<Appointment>>(JSONTextAppointments, settings);
+            return Appointments;
+        }
+    }
+}
