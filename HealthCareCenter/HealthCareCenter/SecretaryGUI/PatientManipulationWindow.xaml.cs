c741cddd615ea3ce57bcda38f3570e129e0c2a11
Vuk Radmilović <vuk.radmilovic01@gmail.com>
2022-04-24 22:11:04 +0200

    Add PatientManipulationWindow structure, working table
    PatientManipulationWindow now has 6 buttons for future implementation, 2 radio buttons to display either all patients or only blocked ones and a table that displays the patients.
    Added 1 more health record and patient for testing.

--- HealthCareCenter/HealthCareCenter/SecretaryGUI/PatientManipulationWindow.xaml.cs
+++ HealthCareCenter/HealthCareCenter/SecretaryGUI/PatientManipulationWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using HealthCareCenter.Model;
 
 namespace HealthCareCenter.SecretaryGUI
 {
@@ -17,9 +18,40 @@ namespace HealthCareCenter.SecretaryGUI
     /// </summary>
     public partial class PatientManipulationWindow : Window
     {
+        private List<Patient> _blockedPatients;
+
+        private void LoadBlockedPatients()
+        {
+            _blockedPatients = new List<Patient>();
+            foreach (Patient patient in UserManager.Patients)
+            {
+                if (patient.IsBlocked)
+                {
+                    _blockedPatients.Add(patient);
+                }
+            }
+        }
+
         public PatientManipulationWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            patientsDataGrid.ItemsSource = UserManager.Patients;
+            patientsDataGrid.IsReadOnly = true;
+            LoadBlockedPatients();
+        }
+
+        private void ShowEveryoneRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            patientsDataGrid.ItemsSource = UserManager.Patients;
+        }
+
+        private void ShowOnlyBlockedRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            patientsDataGrid.ItemsSource = _blockedPatients;
         }
     }
 }
