f25ecd0d5954b283079f9e892034370b4be5705a
MilosS019 <milosstanojlovic019@gmail.com>
2022-06-03 23:38:24 +0200

    Class prescription was missing healthRecordID field

--- HealthCareCenter/HealthCareCenter/Model/Prescription.cs
+++ HealthCareCenter/HealthCareCenter/Model/Prescription.cs
@@ -7,14 +7,24 @@ namespace HealthCareCenter.Model
     public class Prescription
     {
         public Prescription() { }
-        public Prescription(int _id, int _doctorID, List<int> _medicineInstructionsID) {
-            this.ID = _id; 
+        public Prescription(int _id, int _doctorID, List<int> _medicineInstructionsID)
+        {
+            this.ID = _id;
             this.DoctorID = _doctorID;
             this.MedicineInstructionIDs = _medicineInstructionsID;
+        }
+
+        public Prescription(int _id, int _doctorID, List<int> _medicineInstructionsID, int _healthRecordID)
+        {
+            this.ID = _id;
+            this.DoctorID = _doctorID;
+            this.MedicineInstructionIDs = _medicineInstructionsID;
+            this.HealthRecordID = _healthRecordID;
         }
         public int ID { get; set; }
         public int DoctorID { get; set; }
         public List<int> MedicineInstructionIDs { get; set; }
+        public int HealthRecordID { get; set; }
 
     }
 }
